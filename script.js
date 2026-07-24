3b41a80eb47ae49e72e9ddfaad2ad9ec3947d8ea
Command Casual <franci.boi19@gmail.com>
2026-07-24 19:12:27 +1200

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function generate_output_command() {
     final_command += always_active + ",Command:\"";
 
     // get command
-    let command = data.get('command').trim().replaceAll("\\", "\\\|").replaceAll('"', '\\"');
+    let command = data.get('command').trim().replaceAll("\\", "\\\\").replaceAll('"', '\\"');
 
     final_command += command + "\"},Passengers:[{id:armor_stand,Health:0,Passengers:[{id:falling_block,BlockState:{Name:";
 
